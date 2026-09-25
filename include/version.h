938bd261b3a8417926a57ea9960809cc9bc43281
softrider70 <anet64guido@t-online.de>
2026-09-25 16:03:14 +0200

    Build-Nummer auf 96

--- include/version.h
+++ include/version.h
@@ -5,6 +5,6 @@
 
 #define APP_VERSION_MAJOR      0
 #define APP_VERSION_MINOR      1
-#define BUILD_NUMBER           92
-#define BUILD_TIMESTAMP        "2026-09-24 19:38:30"
-#define APP_VERSION_STRING     "v0.1.92"
+#define BUILD_NUMBER           96
+#define BUILD_TIMESTAMP        "2026-09-24 22:45:15"
+#define APP_VERSION_STRING     "v0.1.96"
